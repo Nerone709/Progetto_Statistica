aa2c0eccaf1871c61922f98398ca50b0edb0fb61
nerone709 <napolitanogiuseppe709@gmail.com>
2024-10-22 21:41:22 +0200

    Modifiche al codice, ricerca di colonne con tutti zero

--- Progetto/script.R
+++ Progetto/script.R
@@ -5,9 +5,11 @@ dataset <- read.csv("PhiUSIIL_Phishing_URL_Dataset_4.csv", sep=';')
 zero_columns <- names(dataset)[apply(dataset, 2, function(col) all(col == 0))]
 
 # Verifica se ci sono colonne con tutti zeri
-if (length(zero_columns) == 0) {
+if (length(zero_columns) == 0) 
+{
   print("Non ci sono colonne con tutti valori zero.")
-} else {
+}else 
+  {
   # Plot di tutte le colonne con tutti zeri
   for (col in zero_columns) {
     plot(dataset[[col]], type='h', main=paste('Plot della colonna:', col), xlab='Indice', ylab='Valore')
